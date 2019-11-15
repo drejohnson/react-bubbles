b8bd43b6d2c8c4bd5bcb46561ef62c887e2d9fc9
DeAndre Johnson <dre@phreshr.com>
2019-11-16 00:45:45 +0800

    fix axiosWithAuth

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosWithAuth = () => {
   let token = localStorage.getItem("token");
-  axios.create({
-    url: "http://localhost:5000",
+  return axios.create({
+    baseURL: "http://localhost:5000",
     headers: {
       "Content-Type": "application/json",
       Authorization: token
